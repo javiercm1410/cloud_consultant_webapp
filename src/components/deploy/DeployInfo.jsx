91662fb340dd5a1edf081e1742c38262001f75a5
Javier Carrillo <jamaca1410@gmail.com>
2023-08-01 21:00:13 -0400

    lol

--- src/components/deploy/DeployInfo.jsx
+++ src/components/deploy/DeployInfo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Deploy.module.css';
 import ServicesCard from './ServicesCard';
 import { useLocation } from 'react-router-dom';
@@ -6,33 +6,49 @@ import { FormDataContext } from '../../context/formDataContext';
 
 const DeployInfo = () => {
     // const {formData} = useContext(FormDataContext);
-
+    const { formData } = useContext(FormDataContext);
     const location = useLocation();
     // const data = location.state?.data || location.state?.formData;
     const apiResponse = location.state?.apiResponse || location.state?.Jresp;
     const deploy = location.state?.deployResponse;
     const data = apiResponse?.data;
 
+    let imageSrc = '../../public/aws-logo.png'; // Default image
+    if (formData.cloud_provider === 'AWS' && formData.architecture === 'Classic-three-tier') {
+        imageSrc = '../../public/3TierArch.png'; // Specific image for AWS and exampleArchitecture
+    } else if (formData.cloud_provider === 'GCP' && formData.architecture === 'Container-based') {
+        imageSrc = '../../public/google-cloud-logo.png';
+    }
+
     return(
-        <div className='bg-main vh-100 d-flex flex-column align-items-center'>
+        <div className='bg-main d-flex flex-column align-items-center'>
             <h2 className={`${styles.cloudHeader} `}>Results</h2>
             <div className='row' style={{ width: "80%", marginBottom: "2rem" }}>
                 <div className="col-md-6">
                     <ServicesCard data={data}/>
                 </div>
                 <div className="col-md-6">
-                    <div className={`card ${styles.card}`} style={{ height: '550px', marginBottom: "2rem" }}>   
+                    <div className={`card ${styles.card}`} style={{ height: '500px', marginBottom: "2rem" }}>   
                         <div className="card-header">Endpoints</div>
                         <div className={`card-body bg-dark ${styles.cardBody}`}>
                             <div>
-                                <p>{JSON.stringify(deploy, null, 2)}</p>
+                                <p style={{ whiteSpace: 'pre-line' }}>{deploy}</p>
                             </div>
                             {/* <p className={`${styles.cloudPrice} card-text`}>como asi</p> */}
                         </div>
                     </div>
                 </div>
             </div>
-
+            <div className='row' style={{ width: "80%", marginBottom: "2rem" }}>
+                <div className="col-md-12">
+                    <div className={`card ${styles.card}`} style={{ height: '550px', marginBottom: "2rem" }}>   
+                        <div className="card-header">Detailed diagram</div>
+                        <div className={`card-body bg-dark ${styles.cardBody}`}>
+                            <img src={imageSrc} alt="AWS Logo" style={{ height: '550px', marginTop: "2rem", marginBottom: "2rem" }} className={'.mt-5'}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/deploy/ServicesCard.jsx
+++ src/components/deploy/ServicesCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ServicesCard = ({ data }) => {
   return (
-    <div className="card mb-3">
+    <div className="card mb-3" style={{ height: '500px', marginBottom: "2rem" }}>
       <div className="card-header">Services deployed</div>
       <div className="card-body bg-dark">
         {Object.keys(data).map((service, index) => (
